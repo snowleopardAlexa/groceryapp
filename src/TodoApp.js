3fc49bb2b1bf6f6ef17527d56caed913f40fade6
snowleopardAlexa <alexaslomski@gmail.com>
2021-04-19 21:34:42 -0400

    add material-ui/core, create navbar, create basic setup

---
+++ src/TodoApp.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+
+function TodoApp() {
+  return (
+    <div className="todo">
+       <Paper style={{
+           padding: 0,
+           margin: 0,
+           height: "100vh",
+           backgroundColor: "#fafafa"
+       }}
+       elevation={0}
+       >
+           <AppBar color='primary'  position='static' style={{ height: "64px" }}>
+              <Toolbar>
+                  <Typography color='inherit'>Todos with Hooks</Typography>
+              </Toolbar>
+           </AppBar>
+       </Paper>
+    </div>
+  );
+}
+
+export default TodoApp;
+
+// todoapp: todoform, todolist, todoitem
