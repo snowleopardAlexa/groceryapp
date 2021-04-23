7def8b1e809a1af07ba7f02b9cda0dca9286c957
snowleopardAlexa <alexaslomski@gmail.com>
2021-04-22 21:25:27 -0400

    remove import grid

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -3,7 +3,7 @@ import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Grid from "@material-ui/core/Grid";
+
 
 function TodoApp() {
   return (
