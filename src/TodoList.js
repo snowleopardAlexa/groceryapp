12b19be31ee4c7f8dc854257c5eb2288ebd6cb41
snowleopardAlexa <alexaslomski@gmail.com>
2021-04-23 21:50:23 -0400

    add todo component

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import Todo from "./Todo";
 
 function TodoList(props) {
     return (
@@ -12,9 +11,7 @@ function TodoList(props) {
               <List>
                   {props.todos.map(todo => (
                       <>
-                      <ListItem>
-                          <ListItemText>{todo.task}</ListItemText>
-                      </ListItem>
+                      <Todo task={todo.task} key={todo.id} completed={todo.completed} />
                       <Divider />
                       </>
                   ))}
