bb8c5f68fe33f733c8307695f766d45da7dd2289
snowleopardAlexa <alexaslomski@gmail.com>
2021-04-26 18:50:36 -0400

    add useTodoState.js

---
+++ src/hooks/useTodoState.js
@@ -0,0 +1,9 @@
+// it is not about re-using the content, it is simplification of the TodoApp.js
+// we migrate all hook functions here
+import { useState } from "react";
+
+
+
+export default initialTodos => {
+    
+}
