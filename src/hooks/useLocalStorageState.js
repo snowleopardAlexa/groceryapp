627bc0d3fa98a298b104e7170979eb952eb6dd24
snowleopardAlexa <alexaslomski@gmail.com>
2021-05-02 21:03:52 -0400

    add useEffect

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -13,10 +13,17 @@ function useLocalStorageState(key, defaultVal) {
         }
         return val;
     });
-
     // useEffect to update localstorage when state changes
-
+    useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(state));
+    }, [state]);
+    return [state, setState];
 } 
 
-
 export default useLocalStorageState;
+
+// What try/catch block is:
+// try - lets you test a block of code for errors
+// catch - lets you handle the error
+// throw - lets you create custom errors
+// finally - lets you execute code, after try and catch, regardless the result
