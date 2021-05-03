1346cc285b5514a2a82525b8f558899984558273
snowleopardAlexa <alexaslomski@gmail.com>
2021-05-02 20:41:13 -0400

    add try/catch block, make a piece of state based on value in localstorage

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -1,7 +1,18 @@
 import { useState, useEffect } from 'react';
 
 function useLocalStorageState(key, defaultVal) {
-    // make piece of state, based on value in localstorage
+    // make piece of state, based on value in localstorage (or default)
+    const [state, setState] = useState(() => {
+        let val;
+        try {
+            val = JSON.parse(
+                window.localStorage.getItem(key) || String(defaultValue)
+            );
+        } catch (e) {
+            val = defaultVal;
+        }
+        return val;
+    });
 
     // useEffect to update localstorage when state changes
 
